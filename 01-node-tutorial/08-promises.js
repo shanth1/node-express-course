953bace537c55fbe4f5d82f5e3bcf299f4c90349
Denis Beresnev <shanthi.bunch@gmail.com>
2023-06-01 23:55:04 +0300

    promises for read and write file in fs module
    require("fs").promises works like utils module with promisify
    promisify works like getText function
    getText function works like then-catch statement

---
+++ 01-node-tutorial/08-promises.js
@@ -0,0 +1,41 @@
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+    try {
+        const first = await readFile(
+            "./01-node-tutorial/content/first.txt",
+            "utf8",
+        );
+        const second = await readFile(
+            "./01-node-tutorial/content/second.txt",
+            "utf8",
+        );
+        await writeFile(
+            "./01-node-tutorial/content/result-async.txt",
+            `writeFilePromise: ${first}, ${second}`,
+        );
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, "utf8", (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
+
+// getText("./01-node-tutorial/content/first.txt")
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
