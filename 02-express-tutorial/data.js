23631bfa053cb3794d3512603a73d12a062351f1
Denis Beresnev <shanthi.bunch@gmail.com>
2023-06-07 23:47:29 +0300

    basic json
    added data
    export like module
    used json method in express

---
+++ 02-express-tutorial/data.js
@@ -0,0 +1,13 @@
+const people = [
+    { id: 1, name: "Denis" },
+    { id: 2, name: "Mark" },
+    { id: 3, name: "Bob" },
+];
+
+const product = [
+    { id: 1, type: "food" },
+    { id: 2, type: "drink" },
+    { id: 3, type: "movie" },
+];
+
+module.exports = { product, people };
