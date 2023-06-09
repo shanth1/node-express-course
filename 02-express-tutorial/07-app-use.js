140d28ca2b2cb271b7da9b834cb9a5bb6940d28a
Denis Beresnev <shanthi.bunch@gmail.com>
2023-06-10 00:34:20 +0300

    app use (for middleware)
    refactor code
    middleware for every get requests (or custom) through app.use

---
+++ 02-express-tutorial/07-app-use.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const logger = require("./logger");
+const { products, people } = require("./data");
+
+const app = express();
+
+// app.use(logger);
+app.use("/api", logger);
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About</h1>");
+});
+
+app.get("/contacts", (req, res) => {
+    res.send("<h1>Contacts</h1>");
+});
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
+
+app.get("/api/people", (req, res) => {
+    res.json(people);
+});
+
+app.get("*", (req, res) => {
+    res.send("<h1>Not found</h1>");
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000...");
+});
