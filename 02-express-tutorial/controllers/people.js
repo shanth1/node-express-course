f5b659f1194ed961396f7bd91dbc5b41817ca751
Denis Beresnev <shanthi.bunch@gmail.com>
2023-06-13 11:39:01 +0300

    added controllers to route

---
+++ 02-express-tutorial/controllers/people.js
@@ -0,0 +1,57 @@
+const { people } = require("../../data");
+
+const getPeople = (req, res) => {
+    res.status(200).json({ success: true, data: people });
+};
+
+const addPerson = (req, res) => {
+    const { login } = req.body;
+    if (login) {
+        people.push({ id: Math.random(), name: login });
+        return res.json({ success: true, data: people });
+    }
+    return res.status(400).json({ success: false, msg: "Login is empty" });
+};
+
+const updatePerson = (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const selectedPerson = people.find((person) => person.id === Number(id));
+    if (!selectedPerson) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `No person with id ${id}` });
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name;
+            console.log(person);
+        }
+        return person;
+    });
+
+    res.status(200).send({ success: true, data: newPeople });
+};
+
+deletePerson = (req, res) => {
+    const { id } = req.params;
+
+    const personIndex = people.findIndex((person) => person.id === Number(id));
+    const selectedPerson = people[personIndex];
+
+    if (!selectedPerson) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `No person with id ${id}` });
+    }
+
+    people.splice(personIndex, 1);
+    res.status(200).send({ success: true, data: people });
+};
+
+module.exports = {
+    getPeople,
+    addPerson,
+    updatePerson,
+    deletePerson,
+};
--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,54 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { people } = require("../../data");
+const {
+    getPeople,
+    addPerson,
+    updatePerson,
+    deletePerson,
+} = require("../controllers/people");
 
-router.get("/", (req, res) => {
-    res.status(200).json({ success: true, data: people });
-});
+// router.get("/", getPeople);
+// router.post("/", addPerson);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
-router.post("/", (req, res) => {
-    const { login } = req.body;
-    if (login) {
-        people.push({ id: Math.random(), name: login });
-        return res.json({ success: true, data: people });
-    }
-    return res.status(400).json({ success: false, msg: "Login is empty" });
-});
-
-router.put("/:id", (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
-    const selectedPerson = people.find((person) => person.id === Number(id));
-    if (!selectedPerson) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `No person with id ${id}` });
-    }
-    const newPeople = people.map((person) => {
-        if (person.id === Number(id)) {
-            person.name = name;
-            console.log(person);
-        }
-        return person;
-    });
-
-    res.status(200).send({ success: true, data: newPeople });
-});
-
-router.delete("/:id", (req, res) => {
-    const { id } = req.params;
-
-    const personIndex = people.findIndex((person) => person.id === Number(id));
-    const selectedPerson = people[personIndex];
-
-    if (!selectedPerson) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `No person with id ${id}` });
-    }
-
-    people.splice(personIndex, 1);
-    res.status(200).send({ success: true, data: people });
-});
+router.route("/").get(getPeople).post(addPerson);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
